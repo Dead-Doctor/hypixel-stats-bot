9515b3c2ee6e9ad5ca36ad52f0c57dffdb2082ac
DeadDoctor <bkloth06@gmail.com>
2020-12-26 20:23:46 +0100

    Fixed Bedwars kills

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ client.on('message', function (message) {
               message.reply(`${args[0]} has ${player.achievements.bedwars_wins} wins in BedWars!`);
               break;
             case 'kills':
-              message.reply(`${args[0]} has ${player.achievements.bedwars_wins} kills in BedWars!`);
+              message.reply(`${args[0]} has ${player.achievements.bedwars_bedwars_killer} kills in BedWars!`);
               break;
             case 'final-kills':
               message.reply(`${args[0]} has ${player.stats.final_kills_bedwars} final-kills in BedWars!`);
